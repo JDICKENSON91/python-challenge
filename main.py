5a2ff6c5d92cb3b0a3cd78d768fce502fc979695
Josh <joshua.dickenson91@gmail.com>
2020-06-23 11:45:02 +0800

    Finished Task 1 adding in writing file

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ with open(budget_csv_path) as csvfile:
     budgetcsvreader = csv.reader(csvfile, delimiter=",")
     #Skip first line
     next(budgetcsvreader)
-    pltotal = 0.00
+    pltotal = 0
     #Search through each row
     for row in budgetcsvreader:
         #add profit amount to total
-        pltotal = (float(row[1]) + pltotal)
+        pltotal = (int(row[1]) + pltotal)
          
 print(f"Total: ${pltotal}")
 
@@ -43,7 +43,7 @@ print(f"Total: ${pltotal}")
 
 #Set File Path
 budget_csv_path = os.path.join("Resources", "budget_data.csv")
-LASTvalue = 0.00
+LASTvalue = 0
 ##print (budget_csv_path)
 with open(budget_csv_path) as csvfile:
     budgetcsvreader = csv.reader(csvfile, delimiter=",")
@@ -52,13 +52,13 @@ with open(budget_csv_path) as csvfile:
     #Search through each row
     for row in budgetcsvreader:
         #Find Last Value
-        LASTvalue = (float(row[1]))
+        LASTvalue = (int(row[1]))
 
 #Find First Value
 
 #Set File Path
 budget_csv_path = os.path.join("Resources", "budget_data.csv")
-FIRSTvalue = 0.00
+FIRSTvalue = 0
 SS = 0
 ##print (budget_csv_path)
 with open(budget_csv_path) as csvfile:
@@ -69,15 +69,16 @@ with open(budget_csv_path) as csvfile:
     for row in budgetcsvreader:
         SS == 0
         #Find Last Value
-        FIRSTvalue = (float(row[1]))
+        FIRSTvalue = (int(row[1]))
         if SS == 0:
             break
     
 #print(FIRSTvalue)
 #print(LASTvalue)
 
-avdifftotal = (LASTvalue - FIRSTvalue) /(totalmonths  - 1)
-print(f"Total: ${avdifftotal}")
+avdifftotal = 0
+avdifftotal = int((LASTvalue - FIRSTvalue) /(totalmonths  - 1))
+print(f"Average Change: ${avdifftotal}")
 
 #Set File Path
 budget_csv_path = os.path.join("Resources", "budget_data.csv")
@@ -88,12 +89,12 @@ budget_csv_path = os.path.join("Resources", "budget_data.csv")
 #Scan through list, log prev value, current value and difference. If difference is bigger then last difference  overide current and prev with new values.
 
 
-Maxvalue = 0.00
+Maxvalue = 0
 Dateofmax = ""
-MaxPH = 0.00
-lastvalue = 0.00
-currentvalue = 0.00
-maxchange = 0.00
+MaxPH = 0
+lastvalue = 0
+currentvalue = 0
+maxchange = 0
 
 with open(budget_csv_path) as csvfile:
     budgetcsvreader = csv.reader(csvfile, delimiter=",")
@@ -103,12 +104,12 @@ with open(budget_csv_path) as csvfile:
     for row in budgetcsvreader:
         #see if value is higher than previous value
         lastvalue = currentvalue
-        currentvalue = (float(row[1]))
+        currentvalue = (int(row[1]))
         MaxPH = currentvalue - lastvalue
         if (Maxvalue < MaxPH):
             Dateofmax = (row[0])
             Maxvalue = MaxPH
-            lastvalue = (float(row[1]))
+            lastvalue = (int(row[1]))
 
 print(f"Greatest Increase in Profits: {Dateofmax} (${Maxvalue})")
 
@@ -121,12 +122,12 @@ budget_csv_path = os.path.join("Resources", "budget_data.csv")
 #Scan through list, log prev value, current value and difference. If difference is bigger then last difference  overide current and prev with new values.
 
 
-Minvalue = 0.00
+Minvalue = 0
 Dateofmin = ""
-MinPH = 0.00
-lastvalue = 0.00
-currentvalue = 0.00
-minchange = 0.00
+MinPH = 0
+lastvalue = 0
+currentvalue = 0
+minchange = 0
 
 with open(budget_csv_path) as csvfile:
     budgetcsvreader = csv.reader(csvfile, delimiter=",")
@@ -136,14 +137,32 @@ with open(budget_csv_path) as csvfile:
     for row in budgetcsvreader:
         #see if value is higher than previous value
         lastvalue = currentvalue
-        currentvalue = (float(row[1]))
+        currentvalue = (int(row[1]))
         MinPH = currentvalue - lastvalue
         if (Minvalue > MinPH):
             Dateofmin = (row[0])
             Minvalue = MinPH
-            lastvalue = (float(row[1]))
+            lastvalue = (int(row[1]))
 
 print(f"Greatest Decrease in Profits: {Dateofmin} (${Minvalue})")
+
+#Specify the file to write to
+output_path = os.path.join("analysis", "new.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline="") as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the rows
+    csvwriter.writerow(["Financial Analysis"])
+    csvwriter.writerow(["----------------------------------------"])
+    csvwriter.writerow([f"Total Months: {totalmonths}"])
+    csvwriter.writerow([f"Total: ${pltotal}"])
+    csvwriter.writerow([f"Average Change: ${avdifftotal}"])
+    csvwriter.writerow([f"Greatest Increase in Profits: {Dateofmax} (${Maxvalue})"])
+    csvwriter.writerow([f"Greatest Decrease in Profits: {Dateofmin} (${Minvalue})"])
 
 
 
