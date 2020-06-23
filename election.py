fca5259231f61fcb4863e04cca264968a6b2cd91
Josh <joshua.dickenson91@gmail.com>
2020-06-23 13:55:04 +0800

    updating task 2

--- election.py
+++ election.py
@@ -45,4 +45,46 @@ with open(election_csv_path) as csvfile:
 
 #print(candidates)
 
+#get totals for each candidate
+
+totalfirst = 0
+totalsecond = 0
+totalthird = 0
+totalfourth = 0
+
+#Set File Path
+election_csv_path = os.path.join("Resources", "election_data.csv")
+##print (budget_csv_path)
+with open(election_csv_path) as csvfile:
+    electioncsvreader = csv.reader(csvfile, delimiter=",")
+    #Skip first line
+    next(electioncsvreader)
+    #Search through each row
+    for row in electioncsvreader:
+        #if Candidate is not on list
+        if (row[2]) == (candidates[0]):
+            #append candidates to list
+            totalfirst = (totalfirst +1)
+        if (row[2]) == (candidates[1]):
+            #append candidates to list
+            totalsecond = (totalsecond +1)
+        if (row[2]) == (candidates[2]):
+            #append candidates to list
+            totalthird = (totalthird +1)
+        if (row[2]) == (candidates[3]):
+            #append candidates to list
+            totalfourth = (totalfourth +1)
+
+firstpercent = (int(totalfirst/totalvotes*100))
+secondpercent = (int(totalsecond/totalvotes*100))
+thirdpercent = (int(totalthird/totalvotes*100))
+fourthpercent = (int(totalfourth/totalvotes*100))
+
+
+print(f"{candidates[0]}: {firstpercent}% ({totalfirst})")
+print(f"{candidates[1]}: {secondpercent}% ({totalsecond})")
+print(f"{candidates[2]}: {thirdpercent}% ({totalthird})")
+print(f"{candidates[3]}: {fourthpercent}% ({totalfourth})")
+
+
 
